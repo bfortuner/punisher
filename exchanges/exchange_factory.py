907efc7cf67cac700e293a5353ac8719d63aab3a
Colin Fortuner <cfortuner@riotgames.com>
2018-01-06 00:56:41 -0500

    eh changed my mind lol

--- exchanges/exchange_factory.py
+++ exchanges/exchange_factory.py
@@ -2,25 +2,33 @@ import config as cfg
 from live_exchange import LiveExchange
 from simulated_exchange import SimulatedExchange
 
-def get_exchanges(exchange_ids):
-    exchanges = []
-    for id in exchange_ids:
-        exchange = get_exchange(id)
-        if exchange:
-            print("Unable to get Exchange {}".format(id))
-        else:
-            exchanges.append(exchange)
+class ExchangeFactory():
 
-def get_exchange(exchange_id):
+    def __init__(self, simulated=True):
+        pass
 
-    exchange_cfg = load_exchange_cfg(exchange_id)
+    def get_exchanges(self, exchange_ids):
+        exchanges = []
+        for id in exchange_ids:
+            exchange = get_exchange(id)
+            if exchange:
+                print("Unable to get Exchange {}".format(id))
+            else:
+                exchanges.append(exchange)
 
-    # TODO: Check if exchange exists in ccxt
+    def get_exchange(self, exchange_id):
 
-    return LiveExchange(exchange_id, exchange_cfg)
+        exchange_cfg = self._load_exchange_cfg(exchange_id)
+
+        # TODO: Check if exchange exists in ccxt
+
+        if simulated:
+            return SimulatedExchange(exchange_id, exchange_cfg)
+
+        return LiveExchange(exchange_id, exchange_cfg)
 
 
-def load_exchange_cfg(exchange_id):
-    # TODO: figure out how we want to load configs for exchanges
-    pass
+    def _load_exchange_cfg(self, exchange_id):
+        # TODO: figure out how we want to load configs for exchanges
+        pass
 
