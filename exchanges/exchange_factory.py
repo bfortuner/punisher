60168b616277ec4602b28e4ee338fabb56a41057
Colin Fortuner <cfortuner@riotgames.com>
2018-01-06 00:54:00 -0500

    removing class from exhange factory code, it's sufficient to just make it a module

--- exchanges/exchange_factory.py
+++ exchanges/exchange_factory.py
@@ -2,30 +2,25 @@ import config as cfg
 from live_exchange import LiveExchange
 from simulated_exchange import SimulatedExchange
 
-class ExchangeFactory():
+def get_exchanges(exchange_ids):
+    exchanges = []
+    for id in exchange_ids:
+        exchange = get_exchange(id)
+        if exchange:
+            print("Unable to get Exchange {}".format(id))
+        else:
+            exchanges.append(exchange)
 
-    def __init__(self):
-        pass
+def get_exchange(exchange_id):
 
-    def get_exchanges(self, exchange_ids):
-        exchanges = []
-        for id in exchange_ids:
-            exchange = get_exchange(id)
-            if exchange:
-                print("Unable to get Exchange {}".format(id))
-            else:
-                exchanges.append(exchange)
+    exchange_cfg = load_exchange_cfg(exchange_id)
 
-    def get_exchange(self, exchange_id):
+    # TODO: Check if exchange exists in ccxt
 
-        exchange_cfg = self._load_exchange_cfg(exchange_id)
-
-        # TODO: Check if exchange exists in ccxt
-
-        return LiveExchange(exchange_id, exchange_cfg)
+    return LiveExchange(exchange_id, exchange_cfg)
 
 
-    def _load_exchange_cfg(self, exchange_id):
-        # TODO: figure out how we want to load configs for exchanges
-        pass
+def load_exchange_cfg(exchange_id):
+    # TODO: figure out how we want to load configs for exchanges
+    pass
 
